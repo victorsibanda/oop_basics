e4b60a8113ec4b13ed1b4240934d58ac1e6334c5
Victor Sibanda <victormsibanda1@gmail.com>
2020-03-06 16:02:43 +0000

    super()

--- reptile_class.py
+++ reptile_class.py
@@ -1,16 +1,24 @@
 from animal_class import *
 
+
 class Reptile(Animal):
     pass
+
+    def __init__(self,name,age,colour,scales,blood_temp):
+        super().__init__ (name, age, colour)
+        self.scales = scales
+        self.blood_temp = blood_temp
+
 
     def make_sound(self):
         return ' not woof'
 
 
+
 animal1 = Animal('Nacho',30,'redish')
 
 
-reptile1 = Reptile('Crocodile',16,'greenish')
+reptile1 = Reptile('Crocodile',16,'greenish','lots','very cold')
 
 
 print(animal1)
@@ -21,4 +29,16 @@ print(reptile1.potty())
 print(reptile1.sleep())
 print(reptile1.reproduce())
 print(reptile1.make_sound())
-print (animal1.make_sound())
+print (reptile1.blood_temp())
+
+
+
+
+
+
+
+
+
+
+# for n in range(100):
+#     print ("Fizz"*(not n % 3) + "Buzz"*(not n % 5) or n)
